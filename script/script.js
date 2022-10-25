fd0477e9cfea86a989fb109444af24c01e35402b
durbonca <durbonca@gmail.com>
2022-10-25 16:21:32 -0300

    second commit

--- script/script.js
+++ script/script.js
@@ -1,16 +1,37 @@
 const send_evaluations = (e) => {
     e.preventDefault()
     if (evaluations_number.value >= 1) {
+        // delete all in form and create new inputs
+        promediations.innerHTML = '' // delete all in form
         for( let i = 0; i < evaluations_number.value; i++){
-            const newDiv = document.createElement("input", );
-            const newContent = document.createTextNode("Hi there and greetings!");
-            newDiv.appendChild(newContent);
-            promediations.appendChild(newDiv);
+            // create input type number
+            const inputPromediations = document.createElement("input");
+            inputPromediations.type = 'number';
+            inputPromediations.name = `input_promediations_${i+1}`;
+            inputPromediations.className = 'input_promediations';
+            promediations.appendChild(inputPromediations);
         }
+        // create button
+        const button = document.createElement("input");
+        button.type = 'button';
+        button.value = 'Calcular';
+        button.name = 'button_calculate';
+        button.onclick = promedio; // add function to button
+        promediations.appendChild(button); // add text to button
     }
 }
 
 const promedio = () => {
+    let sum = 0;
+    const inputs = document.querySelectorAll('input[class="input_promediations"]'); // get all inputs
+    inputs.forEach((input) => {
+        sum += parseInt(input.value);
+    })
+    const promedio = sum / inputs.length;
+    //create element
+    const p = document.createElement("p"); // create p
+    p.innerHTML = `El promedio es: ${promedio}`; // add text to p
+    promediations.appendChild(p);
 }
 
 const button_evaluations = document.querySelector('#evaluations_send');
